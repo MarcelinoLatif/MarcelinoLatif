767f0fc61180555e1ae8014b8d803d98004981bc
Your Name <you@example.com>
2026-05-02 23:25:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MarcelinoLatif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to islam’s Carpet Cleaning Service!");
+            Console.WriteLine("Our price list is 25$ for small rooms and 35$ for large ones, 6% Taxes");
+
+            Console.WriteLine("Please, enter the large rooms u want to clean:");
+            int x = Convert.ToInt32(Console.ReadLine()) * 35;
+
+            Console.WriteLine("Please, enter the small rooms u want to clean:");
+            int y = Convert.ToInt32(Console.ReadLine()) * 25;
+
+            double h = x + y;
+            double g = h * 0.06;
+            double t = h + g;
+
+            Console.WriteLine($"Cost = {h}, Tax = {g}, Total = {t}");
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+}
